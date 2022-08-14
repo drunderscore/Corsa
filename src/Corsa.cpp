ac213bf97a03002063f0fe2d8755332fc325d4d9
James Puleo <james@jame.xyz>
2022-08-14 09:10:42 -0400

    Initial commit!

---
+++ src/Corsa.cpp
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2022, James Puleo <james@jame.xyz>
+ *
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+
+#include "Corsa.h"
+
+namespace Corsa
+{
+std::map<SteamAPICall_t, std::function<void(void*)>> s_callbacks_for_async_calls;
+std::map<int, std::function<void(void*)>> s_callbacks_for_callbacks;
+
+void initialize() { SteamAPI_ManualDispatch_Init(); }
+
+void update()
+{
+    SteamAPI_ManualDispatch_RunFrame(SteamAPI_GetHSteamPipe());
+
+    CallbackMsg_t callback_message{};
+    while (SteamAPI_ManualDispatch_GetNextCallback(SteamAPI_GetHSteamPipe(), &callback_message))
+    {
+        if (callback_message.m_iCallback == SteamAPICallCompleted_t::k_iCallback)
+        {
+            auto& steam_api_call_completed = reinterpret_cast<SteamAPICallCompleted_t&>(*callback_message.m_pubParam);
+
+            auto maybe_callback_iterator = s_callbacks_for_async_calls.find(steam_api_call_completed.m_hAsyncCall);
+            if (maybe_callback_iterator == s_callbacks_for_async_calls.end())
+                continue; // We don't have a callback for this async call, so just ignore it.
+
+            char callback_data_bytes[steam_api_call_completed.m_cubParam];
+            bool failed;
+            if (SteamUtils()->GetAPICallResult(steam_api_call_completed.m_hAsyncCall, &callback_data_bytes,
+                                               sizeof(callback_data_bytes), steam_api_call_completed.m_iCallback,
+                                               &failed) &&
+                !failed)
+            {
+                maybe_callback_iterator->second(callback_data_bytes);
+            }
+            else
+            {
+                // FIXME: Would be nice to propagate the failure reason through here.
+                //        I also don't like the thought of null representing failure...
+                maybe_callback_iterator->second(nullptr);
+            }
+
+            s_callbacks_for_async_calls.erase(steam_api_call_completed.m_hAsyncCall);
+        }
+        else if (auto callback_iterator = s_callbacks_for_callbacks.find(callback_message.m_iCallback);
+                 callback_iterator != s_callbacks_for_callbacks.end())
+        {
+            callback_iterator->second(callback_message.m_pubParam);
+        }
+        SteamAPI_ManualDispatch_FreeLastCallback(SteamAPI_GetHSteamPipe());
+    }
+}
+}
---
+++ src/Corsa.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2022, James Puleo <james@jame.xyz>
+ *
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+
+#pragma once
+
+#include <functional>
+#include <map>
+#include <steam/steam_api.h>
+
+namespace Corsa
+{
+extern std::map<SteamAPICall_t, std::function<void(void*)>> s_callbacks_for_async_calls;
+extern std::map<int, std::function<void(void*)>> s_callbacks_for_callbacks;
+
+void initialize();
+void update();
+
+template<typename TCallbackType>
+void await(SteamAPICall_t steam_api_call, std::function<void(TCallbackType*)>&& callback)
+{
+    s_callbacks_for_async_calls[steam_api_call] = std::move(*reinterpret_cast<std::function<void(void*)>*>(&callback));
+}
+
+// TODO: Might be nice if this had a requires clause for TCallbackType having k_iCallback
+// FIXME: Should we allow multiple listeners? Is that an expectation of an API that looks like this?
+template<typename TCallbackType>
+void listen(std::function<void(TCallbackType*)>&& callback)
+{
+    s_callbacks_for_callbacks[TCallbackType::k_iCallback] =
+        std::move(*reinterpret_cast<std::function<void(void*)>*>(&callback));
+}
+
+void remove_listener(int callback_id) { s_callbacks_for_callbacks.erase(callback_id); }
+
+template<typename TCallbackType>
+void remove_listener()
+{
+    remove_listener(TCallbackType::k_iCallback);
+}
+}
